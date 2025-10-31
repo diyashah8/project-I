c8652d8b85b7b6278a5a26e000d9ab9d318dbde9
Diya Shah <cclloouudd19@gmail.com>
2025-10-31 19:01:25 +0545

    day 2

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Frostique</h1>
+    <p>Luxury Warmth <span class="forthe">for the</span> Modern You</p>
+    <form action="">
+        <input type="text" placeholder="phone number,username,email"><br><br>
+        <input type="text" placeholder="password"><br><br>
+        <button>Login</button>
+    </form>
+</body>
+</html>
