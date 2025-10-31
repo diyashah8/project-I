fa543dd73330e6bbb0981d8003651c2277f8646d
Diya Shah <cclloouudd19@gmail.com>
2025-10-31 21:10:13 +0545

    day 2

--- login.php
+++ login.php
@@ -5,13 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+.container{
+    text-align: center;
+    padding: 10px 20px;
+    
+}
+ .forthe{
+        color: red;
+    }
+    .ph, .password{
+        background-color: D9D9D9;
+    
+    }
+    .button{
+        background-color: BAC7D4;
+    }
+</style>
 <body>
+    <div class="container">
     <h1>Frostique</h1>
-    <p>Luxury Warmth <span class="forthe">for the</span> Modern You</p>
-    <form action="">
-        <input type="text" placeholder="phone number,username,email"><br><br>
-        <input type="text" placeholder="password"><br><br>
+    <p1>Luxury Warmth </p>                   <span class="forthe">for the</span> 
+                                                                         <p>Modern You</p>
+    <form class="form">
+        <input type="text" class="ph" placeholder="ph.no,username or email"><br><br>
+        <input type="text" class="password" placeholder="password"><br><br>
         <button>Login</button>
     </form>
+    <p>Forget password?</p>
+    <p>Don't have an account? <a href="signup.php" class="sign up">sign up</a></p>
+    </div>
 </body>
 </html>
