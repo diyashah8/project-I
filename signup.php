9ddc023203af7d497ca9c434b27e30fbc962d362
Diya Shah <cclloouudd19@gmail.com>
2025-11-01 16:46:18 +0545

    day 2

---
+++ signup.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    .container{
+         text-align: center;
+    padding: 0px 20px;
+    font-size: large;
+    }
+    .form{
+background-color:rgba(217, 217, 217, 1); 
+        border: none;
+         width: 400px;   
+  height: 20px; 
+  padding: 5px 10px;
+  font-size: 16px;
+  border-radius: 3px;
+    }
+    
+    input::placeholder{
+        color: black;
+        font-size: small;
+        text-align: left;
+    }
+.country{
+    background-color:rgba(217, 217, 217, 1); 
+    border: none;
+         width: 420px;   
+  height: 30px; 
+  padding: 5px 10px;
+  font-size: 16px;
+  border-radius: 3px;
+}
+    .signup{
+        background-color: rgba(186, 199, 212, 1);
+        border: none;
+         width: 420px;   
+  height: 45px; 
+  padding: 5px 10px;
+  font-size: 16px;
+  border-radius: 3px;
+    }
+</style>
+<body>
+    <div class="container">
+        <h1>Sign up</h1>
+    <form type="form">
+<input type="text" class="form" placeholder="First name" required><br><br>
+        <input type="text" class="form" placeholder="Last name"required><br><br>
+       
+        <input type="text" class="form" placeholder="Email" required><br><br>
+        <input type="date" class="form" placeholder="Birthdate"><br><br>
+        <select id="country" class="country" name="country">
+  <option value="">Country</option>
+  <option value="np">Nepal</option>
+  <option value="in">India</option>
+  <option value="us">United States</option>
+  <option value="uk">United Kingdom</option>
+  <option value="ca">Canada</option>
+  <option value="au">Australia</option>
+</select><br><br>
+ <input type="number" class="form" placeholder="Number" required><br><br>
+  <input type="password" class="form" placeholder="Password" name="password" required><br><br>
+  <input type="password" class="form" placeholder="Confirm_password" name="confirm_password" required><br><br>
+        <button class="signup">Sign up</button>
+    </form>
+    <p>Already have an account? <a href="Login.php" class="login">Login</a></p>
+    </div>
+</body>
+</html>
